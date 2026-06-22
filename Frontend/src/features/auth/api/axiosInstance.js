0f00f6212a40f7be93dd7aa82a8310ad2ab661d6
Rishabh Tivari <rishabhtivari428@gmail.com>
2026-06-22 12:24:53 +0530

    FIXED

--- Frontend/src/features/auth/api/axiosInstance.js
+++ Frontend/src/features/auth/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? "http://localhost:3000" : "https://productivity-dashboard-hp2x.onrender.com"),
+    baseURL: "https://productivity-dashboard-hp2x.onrender.com",
     withCredentials: true
 })
 
